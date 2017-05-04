e402f9071c3d4aa6b72b7d4035ad20f861048869
ma-renaud <ma.renaud@slashvoid.com>
2017-05-04 15:54:43 -0400

    Serial config save, no validation yet.

--- application.py
+++ application.py
@@ -74,15 +74,15 @@ class Application(Gtk.Application):
         Gtk.Application.do_shutdown(self)
 
         #voir https://www.blog.pythonlibrary.org/2013/10/25/python-101-an-intro-to-configparser/
-        config = configparser.ConfigParser()
-        config.set("Serial", "Line", "COM1")
-        config.set("Serial", "Speed", "9600")
-        config.set("Serial", "Data Bits", "8")
-        config.set("Serial", "Stop Bits", "1")
-        config.set("Serial", "Parity", "0")
-        config.set("Serial", "Flow", "2")
+        config = self.get_config()
+        config.set("Serial", "Line", self.serial_config.line)
+        config.set("Serial", "Speed", str(self.serial_config.speed))
+        config.set("Serial", "Data Bits", str(self.serial_config.data_bits))
+        config.set("Serial", "Stop Bits", str(self.serial_config.stop_bits))
+        config.set("Serial", "Parity", str(self.serial_config.parity))
+        config.set("Serial", "Flow", str(self.serial_config.flow))
 
-        with open(self.config_path, "at", encoding='utf8') as config_file:
+        with open(self.config_path, "wt", encoding='utf8') as config_file:
             config.write(config_file)
 
     def do_activate(self):
@@ -105,25 +105,30 @@ class Application(Gtk.Application):
         config.set("Serial", "Parity", "0")
         config.set("Serial", "Flow", "2")
 
-        with open(self.config_path, "at", encoding='utf8') as config_file:
+        with open(self.config_path, "wt", encoding='utf8') as config_file:
             config.write(config_file)
+
+    def get_config(self):
+        if not os.path.exists(self.config_path):
+            self.create_config()
+
+        config = configparser.ConfigParser()
+        config.read(self.config_path)
+        return config
 
     def read_config(self):
         if not self.serial_config:
             self.serial_config = SerialConfig()
 
-        if not os.path.exists(self.config_path):
-            self.create_config()
+        config = self.get_config()
+        config.read('config.ini')
 
-        parser = configparser.ConfigParser()
-        parser.read('config.ini')
-
-        self.serial_config.line = parser.get("Serial", "Line", fallback="COM1")
-        self.serial_config.speed = parser.getint("Serial", "Speed", fallback="9600")
-        self.serial_config.data_bits = parser.getint("Serial", "Data Bits", fallback="8")
-        self.serial_config.stop_bits = parser.getint("Serial", "Stop Bits", fallback="1")
-        self.serial_config.parity = parser.getint("Serial", "Parity", fallback="0")
-        self.serial_config.flow = parser.getint("Serial", "Flow", fallback="2")
+        self.serial_config.line = config.get("Serial", "Line", fallback="COM1")
+        self.serial_config.speed = config.getint("Serial", "Speed", fallback="9600")
+        self.serial_config.data_bits = config.getint("Serial", "Data Bits", fallback="8")
+        self.serial_config.stop_bits = config.getint("Serial", "Stop Bits", fallback="1")
+        self.serial_config.parity = config.getint("Serial", "Parity", fallback="0")
+        self.serial_config.flow = config.getint("Serial", "Flow", fallback="2")
 
     def on_serial_config(self, action, param):
         win = SerialWindow(self.serial_config)
--- serial_window.py
+++ serial_window.py
@@ -2,13 +2,11 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 
-from serial_config import SerialConfig
-
-
 class SerialWindow(Gtk.Window):
 
     def __init__(self, serial_config):
         Gtk.Window.__init__(self, title="Serial Communication Settings")
+        self.serial_config = serial_config
 
         self.set_default_size(400, 300)
         self.set_border_width(15)
@@ -22,52 +20,52 @@ class SerialWindow(Gtk.Window):
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         box_outer.pack_start(hbox, False, True, 0)
         label = Gtk.Label("Serial line to connect to", xalign=0)
-        ed_comm_line = Gtk.Entry()
-        ed_comm_line.set_text(serial_config.line)
+        self.ed_comm_line = Gtk.Entry()
+        self.ed_comm_line.set_text(serial_config.line)
         hbox.pack_start(label, True, True, 0)
-        hbox.pack_start(ed_comm_line, False, True, 0)
+        hbox.pack_start(self.ed_comm_line, False, True, 0)
 
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         box_outer.pack_start(hbox, False, True, 0)
         label = Gtk.Label("Speed (baud)", xalign=0)
-        ed_speed = Gtk.Entry()
-        ed_speed.set_text(str(serial_config.speed))
+        self.ed_speed = Gtk.Entry()
+        self.ed_speed.set_text(str(serial_config.speed))
         hbox.pack_start(label, True, True, 0)
-        hbox.pack_start(ed_speed, False, True, 0)
+        hbox.pack_start(self.ed_speed, False, True, 0)
 
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         box_outer.pack_start(hbox, False, True, 0)
         label = Gtk.Label("Data bits", xalign=0)
-        ed_data = Gtk.Entry()
-        ed_data.set_text(str(serial_config.data_bits))
+        self.ed_data = Gtk.Entry()
+        self.ed_data.set_text(str(serial_config.data_bits))
         hbox.pack_start(label, True, True, 0)
-        hbox.pack_start(ed_data, False, True, 0)
+        hbox.pack_start(self.ed_data, False, True, 0)
 
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         box_outer.pack_start(hbox, False, True, 0)
         label = Gtk.Label("Stop bits", xalign=0)
-        ed_stop = Gtk.Entry()
-        ed_stop.set_text(str(serial_config.stop_bits))
+        self.ed_stop = Gtk.Entry()
+        self.ed_stop.set_text(str(serial_config.stop_bits))
         hbox.pack_start(label, True, True, 0)
-        hbox.pack_start(ed_stop, False, True, 0)
+        hbox.pack_start(self.ed_stop, False, True, 0)
 
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         box_outer.pack_start(hbox, False, True, 0)
         label = Gtk.Label("Parity", xalign=0)
-        combo_parity = Gtk.ComboBoxText()
-        self.fill_combo_parity(combo_parity)
-        combo_parity.set_active_id(str(serial_config.parity))
+        self.combo_parity = Gtk.ComboBoxText()
+        self.fill_combo_parity(self.combo_parity)
+        self.combo_parity.set_active_id(str(serial_config.parity))
         hbox.pack_start(label, True, True, 0)
-        hbox.pack_start(combo_parity, False, True, 0)
+        hbox.pack_start(self.combo_parity, False, True, 0)
 
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
         box_outer.pack_start(hbox, False, True, 0)
         label = Gtk.Label("Flow control", xalign=0)
-        combo_flow = Gtk.ComboBoxText()
-        self.fill_combo_flow(combo_flow)
-        combo_flow.set_active_id(str(serial_config.flow))
+        self.combo_flow = Gtk.ComboBoxText()
+        self.fill_combo_flow(self.combo_flow)
+        self.combo_flow.set_active_id(str(serial_config.flow))
         hbox.pack_start(label, True, True, 0)
-        hbox.pack_start(combo_flow, False, True, 0)
+        hbox.pack_start(self.combo_flow, False, True, 0)
 
         vbox = Gtk.VBox(False, 5)
         hbox = Gtk.HBox(True, 3)
@@ -75,12 +73,14 @@ class SerialWindow(Gtk.Window):
         valign = Gtk.Alignment(xalign=0.0, yalign=1.0, xscale=0.0, yscale=0.0)
         vbox.pack_start(valign, True, True, 0)
 
-        ok = Gtk.Button("Save")
-        ok.set_size_request(70, 30)
-        close = Gtk.Button("Cancel")
+        bt_save = Gtk.Button("Save")
+        bt_save.set_size_request(70, 30)
+        bt_save.connect("clicked", self.on_save_clicked)
+        bt_cancel = Gtk.Button("Cancel")
+        bt_cancel.connect("clicked", self.on_cancel_clicked)
 
-        hbox.add(ok)
-        hbox.add(close)
+        hbox.add(bt_save)
+        hbox.add(bt_cancel)
 
         halign = Gtk.Alignment(xalign=1.0, yalign=0.0, xscale=0.0, yscale=0.0)
         halign.add(hbox)
@@ -90,18 +90,32 @@ class SerialWindow(Gtk.Window):
 
         self.show_all()
 
-    def fill_combo_parity(self, combo):
+    @staticmethod
+    def fill_combo_parity(combo):
         combo.insert(0, "0", "None")
         combo.insert(1, "1", "Odd")
         combo.insert(2, "2", "Even")
         combo.insert(3, "3", "Mark")
         combo.insert(4, "4", "Space")
 
-    def fill_combo_flow(self, combo):
+    @staticmethod
+    def fill_combo_flow(combo):
         combo.insert(0, "0", "None (DTR/RTS enable)")
         combo.insert(1, "1", "None (DTR/RTS disable)")
         combo.insert(2, "2", "XON/XOFF")
         combo.insert(3, "3", "RTS/CTS")
+
+    def on_save_clicked(self, button):
+        self.serial_config.line = self.ed_comm_line.get_text()
+        self.serial_config.speed = int(self.ed_speed.get_text())
+        self.serial_config.data_bits = int(self.ed_data.get_text())
+        self.serial_config.stop_bits = int(self.ed_stop.get_text())
+        self.serial_config.parity = int(self.combo_parity.get_active_id())
+        self.serial_config.flow = int(self.combo_flow.get_active_id())
+        self.close()
+
+    def on_cancel_clicked(self, button):
+        self.close()
 
 
 if __name__ == "__main__":
